52b7e8318374978ea0b5ff701ec0c8e6eef197a0
Your Name <leonardo.tredese@hotmail.com>
2022-10-24 08:57:16 +0200

    Substituted HashableArray with Tuple

--- lab1/solution.py
+++ lab1/solution.py
@@ -16,39 +16,6 @@ def problem(N: int, seed=None):
         for n in range(random.randint(N, N * 5))
     ]
 
-class HashableArray:
-    def __init__(self, data: np.ndarray):
-        self._data = np.sort(data.copy())
-        self._data.flags.writeable = False
-
-    def __hash__(self):
-        return hash(bytes(self._data))
-
-    def __eq__(self, other):
-        return bytes(self._data) == bytes(other._data)
-
-    def __lt__(self, other):
-        return bytes(self._data) < bytes(other._data)
-
-    def __str__(self):
-        return 'h'+str(self._data)
-
-    def __repr__(self):
-        return 'h'+repr(self._data)
-
-    def __len__(self):
-        return len(self._data)
-
-    def __iter__(self):
-        return iter(self._data)
-
-    @property
-    def data(self):
-        return self._data
-
-    def copy_data(self):
-        return self._data.copy()
-
 def search(
     initial_state: frozenset,
     goal_test: Callable,
@@ -98,7 +65,7 @@ def gen_possible_actions(problem_input: list) -> Callable:
         Returns a function that generates all the possible actions
         for problem_input
     """
-    PROBLEM = sorted(set(map(lambda x: HashableArray(np.array(x)), problem_input)))
+    PROBLEM = sorted(set(map(lambda x: tuple(x), problem_input)))
     def possible_actions(state: frozenset) -> Generator[int, None, None]:
         """
            Returns a generator that outputs all the possible
@@ -108,7 +75,7 @@ def gen_possible_actions(problem_input: list) -> Callable:
     return possible_actions
 
 
-def result(state: frozenset, action: HashableArray) -> frozenset:
+def result(state: frozenset, action: tuple) -> frozenset:
     return state.union([action])
 
 def gen_h(N: int) -> Callable:
